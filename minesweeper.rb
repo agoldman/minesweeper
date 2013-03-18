803df3355d41d6b3bcb04316965230ae3a4a5e97
April+Lisa <April+Lisa@appacademy.io>
2013-03-18 13:42:11 -0700

    Initialized bombs and fringes

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,6 +1,6 @@
 class Minesweeper
 
-  attr_accessor :mines, :flagged_spots, :finge_values, :checked_spots
+  attr_accessor :mines, :flagged_spots, :fringe_values, :checked_spots, :row, :col
 
   def initialize(row = 9, col = 9)
     @mines = []
@@ -13,31 +13,46 @@ class Minesweeper
 
     @flagged_spots = []
     @fringe_values = create_fringe
-
+    @row = row
+    @col = col
 
   end
 
 
   def create_fringe
     @mines.each |spot|
-    temp_fringes =[spot[0] - 1, spot[1]]
-    fringe2 =[spot[0] - 1, spot[1] + 1]
-    fringe3 =[spot[0], spot[1] + 1]
-    fringe4 =[spot[0] + 1, spot[1] + 1]
-    fringe5 =[spot[0] + 1, spot[1]]
-    fringe6 =[spot[0] + 1, spot[1] - 1]
-    fringe7 =[spot[0], spot[1] - 1]
-    fringe8 =[spot[0] - 1, spot[1] - 1]
-
-
-
+    #find all 8 spots neighboring a bomb
+      temp_fringes = [[spot[0] - 1, spot[1]],
+                  [spot[0] - 1, spot[1] + 1],
+                  [spot[0], spot[1] + 1],
+                  [spot[0] + 1, spot[1] + 1],
+                  [spot[0] + 1, spot[1]],
+                  [spot[0] + 1, spot[1] - 1],
+                  [spot[0], spot[1] - 1],
+                  [spot[0] - 1, spot[1] - 1]]
+                  #filter for which of those spots are inbounds
+      inbounds_fringes = temp_fringes.select { |current_spot| in_bounds?(current_spot) }
+      add_fringes(inbounds_fringes)
+      end
     end
   end
 
-  def in_bounds?(position, row, col)
+  #add inbounds fringe spots to fringe hash
+  def add_fringes(inbounds_fringes)
+    inbound_fringes.each do |spot|
+      if @fringe_values.key?(spot)
+        @fringe_values[spot] += 1
+      else
+        @fringe_values[spot] = 1
+      end
+    end
+  end
+
+
+  def in_bounds?(position)
     x = position[0]
     y = position[1]
-    0 <= x && x <= row && 0 <= y && y <= col
+    0 <= x && x <= @row && 0 <= y && y <= @col
   end
 
 
